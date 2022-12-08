f21357ccb0e20678cbe0b3192f175b6823c55c35
unknown <abhimvj575@gmail.com>
2022-12-08 13:38:36 +0530

    [Abhilash] Added: finding QuotientAndRemainder

--- BasicCorePrograms/Program.cs
+++ BasicCorePrograms/Program.cs
@@ -8,7 +8,7 @@ namespace BasicCoreProgram
         {
             while (true)
             {
-                Console.WriteLine("1.leapyear\n2.FlipCoin\n3.LargestOfThreeNumber\n4.EvenOrOdd");
+                Console.WriteLine("1.leapyear\n2.FlipCoin\n3.LargestOfThreeNumber\n4.EvenOrOdd\n5.QuotientAndRemainder");
                 int option = Convert.ToInt32(Console.ReadLine());
                 switch (option)
                 {
@@ -16,17 +16,25 @@ namespace BasicCoreProgram
                         LeapYear year = new LeapYear();
                         year.findleap();
                         break;
+
                     case 2:
                         Flipcoin flip = new Flipcoin();
                         flip.CheckHeadAndTail();
                         break;
+
                     case 3:
                         LargestOfThreeNumbers LargeNUmbers = new LargestOfThreeNumbers();
                         LargeNUmbers.FindLargestNum();
                         break;
+
                     case 4:
                         EvenOrOdd num = new EvenOrOdd();
                         num.FindLargestNum();
+                        break;
+
+                    case 5:
+                        QuotientAndRemainder res = new QuotientAndRemainder();
+                        res.Remainder();
                         break;
 
                     default:
---
+++ BasicCorePrograms/QuotientAndRemainder.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCoreProgram
+{
+    internal class QuotientAndRemainder
+    {
+        int num1, num2, remainder, Quotient;
+
+        public void Remainder()
+        {
+            Console.WriteLine("enter the two number");
+            num1 = Convert.ToInt32(Console.ReadLine());
+            num2 = Convert.ToInt32(Console.ReadLine());
+            remainder = num1 % num2;
+            Console.WriteLine("the remainder is =" + remainder);
+            Quotient = num1 / num2;
+            Console.WriteLine("the quotient is =" + Quotient);
+
+
+        }
+    }
+}
