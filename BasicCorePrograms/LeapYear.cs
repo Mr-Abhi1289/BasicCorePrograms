b2767cb4e56ec8354367997137637ca3116060e8
unknown <abhimvj575@gmail.com>
2022-12-08 13:16:51 +0530

    [Abhilash] Added: LeapYear

---
+++ BasicCorePrograms/LeapYear.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCoreProgram
+{
+    internal class LeapYear
+    {
+        public void findleap()
+
+        {
+            Console.WriteLine("enter the year");
+            int year = Convert.ToInt32(Console.ReadLine());
+
+            if (year % 4 == 0)
+            {
+                Console.WriteLine(year + " is a leap year");
+            }
+            else
+            {
+                Console.WriteLine(year + " is not a leapYear");
+            }
+
+        }
+    }
+}
---
+++ BasicCorePrograms/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace BasicCoreProgram
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("1.leapyear");
+                int option = Convert.ToInt32(Console.ReadLine());
+                switch (option)
+                {
+                    case 1:
+                        LeapYear year = new LeapYear();
+                        year.findleap();
+                        break;
+
+                    default:
+                        Console.WriteLine("enter the valid choice");
+                        break;
+                }
+            }
+        }
+    }
+}
+
+
